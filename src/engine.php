35a666e5e8db2e9b619b2a54af1cd0546b0faf15
Dmitriy Zhebrovskiy <dimjeb@gmail.com>
2021-05-24 09:57:20 +0800

    esc

--- src/engine.php
+++ src/engine.php
@@ -15,13 +15,13 @@ function gcd(int $num1, int $num2): int
 }
 
 
-function in($greet)
+function in(string $greet): string
 {
     echo $greet, ' ';
     return trim(fgets(STDIN));
 }
 
-function Progression($length)
+function Progression(int $length): array
 {
     $result = []; // массив с результатами
     $temp = rand(1, 5); // инкремент
@@ -33,7 +33,7 @@ function Progression($length)
     return $result;
 }
 
-function prime($number)
+function prime(int $number): bool
 {
     for ($i = 2; $i < $number; $i++) {
         if ($number % $i == 0) {
