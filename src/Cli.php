0f215e03dfd76f5b08c8cf4a3fbb188897934428
Dmitriy Zhebrovskiy <dimjeb@gmail.com>
2021-05-24 09:27:45 +0800

    esc

--- src/Cli.php
+++ src/Cli.php
@@ -1,10 +1,11 @@
 <?php
 
-function null hello()
+function hello()
 {
      echo 'Welcome to the Brain Game!', PHP_EOL;
      echo 'May I have your name? ';
 
      $name = trim((string)fgets(STDIN));
      echo "Hello, ", $name, "!", PHP_EOL;
+     return null;
 }
